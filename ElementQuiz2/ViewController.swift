3fc842a81fc41d2ae81b69671e6b262bbda5cbb2
Donald van de Weyer <donald@afaik.org>
2020-09-08 10:17:18 +0200

    Initial Commit

---
+++ ElementQuiz2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElementQuiz2
+//
+//  Created by Donald van de Weyer on 08/09/20.
+//  Copyright © 2020 Donald van de Weyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
